ecdb7845adf2559448b4e92adb6c0b2e3832d2b9
papireef478 <kayla.mtschool@gmail.com>
2026-04-23 13:31:36 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -10,8 +10,8 @@ const TIME_ZONE = "America/Chicago";
 const STORAGE_PREFIX = "ssrp_";
 
 // 🔗 Google Apps Script Web App URLs
-const API_URL = "https://script.google.com/macros/s/AKfycbxifDdcTquuF55ARFfd-5LzI-I3H8Tn4nAwv_BoWjdjupgYUXtmelcW_k8RY4f2kq1zug/exec";
-const PENAL_CODE_API_URL = "https://script.google.com/macros/s/AKfycbxifDdcTquuF55ARFfd-5LzI-I3H8Tn4nAwv_BoWjdjupgYUXtmelcW_k8RY4f2kq1zug/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyPYWL6Oqvfwk99lyWCfcgD9xUnaCLYQjwIJSzCH34tE3GrbigV0HoLXUf8O9TMwI620A/exec";
+const PENAL_CODE_API_URL = "https://script.google.com/macros/s/AKfycbyPYWL6Oqvfwk99lyWCfcgD9xUnaCLYQjwIJSzCH34tE3GrbigV0HoLXUf8O9TMwI620A/exec";
 
 // Google Sheets API Key
 const SHEETS_API_KEY = 'AIzaSyAjWST6rOH_rF9Hspvf0j3xI8xdUZ3moYk';
